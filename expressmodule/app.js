e3360b3380716030981b97f563a28aa7d55152be
shivamvk <shivam.vk529@gmail.com>
2019-10-19 11:38:06 +0530

    add bodyparser module

--- expressmodule/app.js
+++ expressmodule/app.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const path = require("path");
-
+const bodyParser = require("body-parser");
 const app = express();
 
 //creating a server with express
 // app.get("/",(req,res)=>{
 //     res.send("Express server up and running!");
-// }).listen(3000);
+// });
 
 //handling url parameters and queries
 // app.get("/example/:name",(req,res)=>{
 //     console.log(req.params);
 //     console.log(req.query);
 //     res.send("Hello " + req.params.name + ". Your age is " + req.query.age);
-// }).listen(3000);
+// });
 
 //serving static content using express
-app.use("/public",express.static(path.join(__dirname,"static")));
+ app.use("/public",express.static(path.join(__dirname,"static")));
+// app.get("/",(req,res)=>{
+//     res.sendFile(path.join(__dirname,"static","index.html"));
+// });
+
+//handling post request and using body-parser module
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"static","index.html"));
-}).listen(3000);
+});
+app.post("/",(req,res)=>{
+    console.log(req.body);
+    //do database works
+    res.send({success: true});
+});
+
+app.listen(3000);
